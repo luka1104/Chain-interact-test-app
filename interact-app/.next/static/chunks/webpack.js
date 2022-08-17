5854a4e8978a964b8625802545c0e86ba9490291
luka1104 <skyward.luke11@gmail.com>
2022-08-18 01:34:56 +0900

    interact with aws

--- interact-app/.next/static/chunks/webpack.js
+++ interact-app/.next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93d8d66d1d9fe9c0"; }
+/******/ 		__webpack_require__.h = function() { return "2b626d6fccdf4291"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- interact-app/src/components/navbar.tsx
+++ interact-app/src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import {
   Box,
   Flex,
@@ -13,11 +14,56 @@ import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [address, setAddress] = useState('')
+
+  const shortenString = (str: string, n: any) => {
+    return (str.length > n) ? str.slice(0, n-1) + '...' : str;
+  }
+
+  const handleWalletConnect = async () => {
+    const result = await (window as any).aptos.connect();
+    if(result.address) {
+      setAddress(result.address)
+    }
+    console.log(result);
+  }
+
+  const checkWalletAddress = async () => {
+    const status = await (window as any).aptos.isConnected()
+    if(status) {
+      const accountAddress = await (window as any).aptos.account()
+      setAddress(accountAddress.address)
+    }
+  }
+
+  const disconnect = async () => {
+    await (window as any).aptos.disconnect()
+  }
+
+  const sendTransaction = async () => {
+    console.log(address);
+    const token = new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString("0x1::aptos_coin::AptosCoin"));
+    const transaction = new TxnBuilderTypes.TransactionPayloadScriptFunction(
+        TxnBuilderTypes.ScriptFunction.natural(
+            "0x1::coin",
+            "transfer",
+            [token],
+            [BCS.bcsToBytes(TxnBuilderTypes.AccountAddress.fromHex('0xc4688d3defda966a30c1fdab7e7f9fa600f81ad65ff39d07933ae4069d8530ca')), BCS.bcsSerializeUint64(1000)],
+        ),
+    );
+    const response = await (window as any).aptos.signAndSubmitTransaction(transaction)
+    // const signedTransaction = await (window as any).aptos.signTransaction(transaction)
+    console.log(response);
+  }
+
+  useEffect(() => {
+    checkWalletAddress()
+  }, [])
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box><Text>Transaction Speed Test</Text></Box>
+          <Box><Text>Interact Test</Text></Box>
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
@@ -31,10 +77,20 @@ const Navbar = () => {
                 _hover={{
                   opacity: '1'
                 }}
-                onClick={()  => (window as any).aptos.connect()}
+                onClick={address ? disconnect : handleWalletConnect}
               >
-                <BiWallet />&nbsp;&nbsp;Connect Wallet
+                <BiWallet />
+                {address ? (
+                  <>
+                    &nbsp;&nbsp;{shortenString(address, 12)}
+                  </>
+                ) : (
+                  <>
+                    &nbsp;&nbsp;Connect Wallet
+                  </>
+                )}
               </Button>
+              <Button onClick={sendTransaction}>test</Button>
             </Stack>
           </Flex>
         </Flex>
